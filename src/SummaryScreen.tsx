33309f5be35b3bf3d0efa915c746cff2b028b042
Naved Islam <navedcoded@gmail.com>
2020-09-23 13:15:28 +0300

    STYLES

--- src/SummaryScreen.tsx
+++ src/SummaryScreen.tsx
@@ -15,25 +15,25 @@ function SummaryScreen({navigation: {dangerouslyGetParent} }) {
           {surahsOld[title].summary.breakdown.map((breakdown, index) => {
             return (
               <View style={styles.bodyContainer}>
-                  {breakdown.details.map((details, index) => {
-                    console.log(details[1])
-                    if (details[1] !== undefined) {
-                      return (
-                        <Text style={styles.yasirText}>
-                          {details[0] + "\n\n"}
-                        </Text>
-                      )
-                    }
-                    else {
-                      return (
-                        <Text style={styles.bodyText}>
-                          {details[0] + "\n\n"}
-                        </Text>
-                      )
-                    }
-                  })}
-                                  
-                <MenuComp name={breakdown.name} content={breakdown.details} surahName={surahsOld[title].name} title={title} index={index}/>
+                <MenuComp breakdown={breakdown} content={breakdown.details} surahName={surahsOld[title].name} title={title} index={index}/>
+
+                {breakdown.details.map((details, index) => {
+                  console.log(details[1])
+                  if (details[1] !== undefined) {
+                    return (
+                      <Text style={styles.yasirText}>
+                        {details[0] + ' (YQ. ' + details[1] + ')'}
+                      </Text>
+                    )
+                  }
+                  else {
+                    return (
+                      <Text style={styles.bodyText}>
+                        {details[0]}
+                      </Text>
+                    )
+                  }
+                })}        
               </View>
             )
         })}
@@ -59,51 +59,33 @@ const styles = StyleSheet.create({
       width: 0,
       height: 1,
     },
+    backgroundColor: '#F2FDE0',
+    paddingLeft: 22,
+    paddingRight: 22,
+    paddingBottom: 10,
     shadowOpacity: 0.12,
     shadowRadius: 3,
-    elevation: 2,
-  },
-  bodyTitle: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-    paddingRight: 15,
-    paddingLeft: 15,
-    backgroundColor: '#F7F7F7',
-    borderRadius: 8,
-    paddingTop: 17,
-    marginTop: -10,
-    zIndex: 0,
-  },
-  bodyTitleText: {
-    color: '#2B4005',
-    fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'right',
-  },
-  bodyTitleButton: {
-    textAlign: 'left',
-    fontWeight: 'bold',
+    elevation: 5,
+    overflow: "hidden",
   },
   bodyText: {
-    fontSize: 16,
-    backgroundColor: '#FFFFFF',
-    padding: 20,
+    fontSize: 17,
+    paddingBottom: 20,
     textAlign: 'left',
     zIndex: 1,
     lineHeight: 22
   },
   yasirText: {
-    fontSize: 16,
-    backgroundColor: '#EEE',
-    padding: 20,
+    fontSize: 17,
+    fontWeight: 'bold',
+    paddingBottom: 20,
     textAlign: 'left',
     zIndex: 1,
     lineHeight: 22
   },
   container: {
     flex: 1,
-    backgroundColor: '#F7F7F7',
+    backgroundColor: '#fff',
     padding: 0,
   },
 })
