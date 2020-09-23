1ee723135a7b57ef22a93c19ec22c70d30a68ed3
Naved Islam <navedcoded@gmail.com>
2020-09-23 18:29:18 +0300

    verse nums

--- src/surahs.js
+++ src/surahs.js
@@ -24,11 +24,11 @@ var surahs = [
 	{
 		title: 'Part 3',
 		data: [	{name: 'Āl-ʿImrān', 			number: 3,
-		pageGreen: 50, period: 'Makkan', verses: 99} ]
+		pageGreen: 50, period: 'Makkan', verses: 200} ]
 	},
 	{
 		title: 'Part 4',
-		data: [	{name: 'an-Nisāʾ',			number: 4,		pageGreen: 77, period: 'Makkan', verses: 99}	]
+		data: [	{name: 'an-Nisāʾ',			number: 4,		pageGreen: 77, period: 'Makkan', verses: 176}	]
 	},
 	{
 		title: 'Part 5',
@@ -37,255 +37,255 @@ var surahs = [
 	{
 		title: 'Part 6',
 		data: [	{name: 'al-Māʾidah', 			number: 5,
-		pageGreen: 106, period: 'Makkan', verses: 99}	]
+		pageGreen: 106, period: 'Makkan', verses: 120}	]
 	},
 	{
 		title: 'Part 7',
 		data: [	{name: 'al-Anʿām', 			number: 6,
-		pageGreen: 128, period: 'Makkan', verses: 99}	]
+		pageGreen: 128, period: 'Makkan', verses: 206}	]
 	},
 	{
 		title: 'Part 8',
 		data: [	{name: 'al-Aʿrāf', 			number: 7,
-		pageGreen: 151, period: 'Makkan', verses: 99}	]
+		pageGreen: 151, period: 'Makkan', verses: 75}	]
 	},
 	{
 		title: 'Part 9',
 		data: [{name: 'al-Anfāl', 			number: 8,
-		pageGreen: 177, period: 'Makkan', verses: 99},]
+		pageGreen: 177, period: 'Makkan', verses: 75},]
 	},
 	{
 		title: 'Part 10',
 		data: [{name: 'at-Taubah', 			number: 9,
-		pageGreen: 187, period: 'Makkan', verses: 99},]
+		pageGreen: 187, period: 'Makkan', verses: 129},]
 	},
 	{
 		title: 'Part 11',
 		data: [{name: 'Yūnus', 			number: 10,
-		pageGreen: 208, period: 'Makkan', verses: 99},
+		pageGreen: 208, period: 'Makkan', verses: 109},
 		{name: 'Hūd', pageGreen: 221, 			number: 11,
-		period: 'Makkan', verses: 99},]
+		period: 'Makkan', verses: 123},]
 	},
 	{
 		title: 'Part 12',
 		data: [
 			{name: 'Yūsuf', pageGreen: 235, 			number: 12,
-			period: 'Makkan', verses: 99},
+			period: 'Makkan', verses: 111},
 		]
 	},
 	{
 		title: 'Part 13',
-		data: [{name: 'ar-Raʿd', 	number: 13,pageGreen: 249, period: 'Makkan', verses: 99},
-		{name: 'Ibrāhīm', 	number: 14,pageGreen: 255, period: 'Makkan', verses: 99},]
+		data: [{name: 'ar-Raʿd', 	number: 13,pageGreen: 249, period: 'Makkan', verses: 43},
+		{name: 'Ibrāhīm', 	number: 14,pageGreen: 255, period: 'Makkan', verses: 52},]
 	},
 	{
 		title: 'Part 14',
 		data: [{name: 'al-Ḥijr', 	number: 15,pageGreen: 262, period: 'Makkan', verses: 99},
-		{name: 'an-Naḥl', 	number: 16,pageGreen: 267, period: 'Makkan', verses: 99},]
+		{name: 'an-Naḥl', 	number: 16,pageGreen: 267, period: 'Makkan', verses: 128},]
 	},
 	{
 		title: 'Part 15',
-		data: [{name: 'al-Isrāʾ', 	number: 17,pageGreen: 282, period: 'Makkan', verses: 99},
-		{name: 'al-Kahf', 	number: 18,pageGreen: 293, period: 'Makkan', verses: 99},]
+		data: [{name: 'al-Isrāʾ', 	number: 17,pageGreen: 282, period: 'Makkan', verses: 111},
+		{name: 'al-Kahf', 	number: 18,pageGreen: 293, period: 'Makkan', verses: 110},]
 	},
 	{
 		title: 'Part 16',
-		data: [{name: 'Maryam', 	number: 19,pageGreen: 305, period: 'Makkan', verses: 99},
-		{name: 'Ṭā-Hā', 	number: 20, pageGreen: 312, period: 'Makkan', verses: 99},]
+		data: [{name: 'Maryam', 	number: 19,pageGreen: 305, period: 'Makkan', verses: 98},
+		{name: 'Ṭā-Hā', 	number: 20, pageGreen: 312, period: 'Makkan', verses: 135},]
 	},
 	{
 		title: 'Part 17',
-		data: [{name: 'al-Anbiyāʾ', 	number: 21, pageGreen: 322, period: 'Makkan', verses: 99},
-		{name: 'al-Ḥajj', 	number: 22, pageGreen: 332, period: 'Makkan', verses: 99},]
+		data: [{name: 'al-Anbiyāʾ', 	number: 21, pageGreen: 322, period: 'Makkan', verses: 112},
+		{name: 'al-Ḥajj', 	number: 22, pageGreen: 332, period: 'Makkan', verses: 78},]
 	},
 	{
 		title: 'Part 18',
-		data: [{name: 'al-Muʾminūn', 	number: 23, pageGreen: 342, period: 'Makkan', verses: 99},
-		{name: 'an-Nūr', 	number: 24, pageGreen: 350, period: 'Makkan', verses: 99},
-		{name: 'al-Furqān', 	number: 25, pageGreen: 359, period: 'Makkan', verses: 99},]
+		data: [{name: 'al-Muʾminūn', 	number: 23, pageGreen: 342, period: 'Makkan', verses: 118},
+		{name: 'an-Nūr', 	number: 24, pageGreen: 350, period: 'Makkan', verses: 64},
+		{name: 'al-Furqān', 	number: 25, pageGreen: 359, period: 'Makkan', verses: 77},]
 	},
 	{
 		title: 'Part 19',
-		data: [{name: 'ash-Shuʿarā', 	number: 26, pageGreen: 367, period: 'Makkan', verses: 99},
-		{name: 'an-Naml', 	number: 27, pageGreen: 377, period: 'Makkan', verses: 99},]
+		data: [{name: 'ash-Shuʿarā', 	number: 26, pageGreen: 367, period: 'Makkan', verses: 227},
+		{name: 'an-Naml', 	number: 27, pageGreen: 377, period: 'Makkan', verses: 93},]
 	},
 	{
 		title: 'Part 20',
-		data: [{name: 'al-Qaṣaṣ', 	number: 28, pageGreen: 385, period: 'Makkan', verses: 99},
-		{name: 'al-ʿAnkabūt', 	number: 29, pageGreen: 396, period: 'Makkan', verses: 99},]
+		data: [{name: 'al-Qaṣaṣ', 	number: 28, pageGreen: 385, period: 'Makkan', verses: 88},
+		{name: 'al-ʿAnkabūt', 	number: 29, pageGreen: 396, period: 'Makkan', verses: 69},]
 	},
 	{
 		title: 'Part 21',
-		data: [{name: 'ar-Rūm', 	number: 30, pageGreen: 404, period: 'Makkan', verses: 99},
-		{name: 'Luqmān', 	number: 31, pageGreen: 411, period: 'Makkan', verses: 99},
-		{name: 'as-Sajdah',  	number: 32, pageGreen: 415, period: 'Makkan', verses: 99},
-		{name: 'al-Aḥzāb',  	number: 33, pageGreen: 418, period: 'Makkan', verses: 99},]
+		data: [{name: 'ar-Rūm', 	number: 30, pageGreen: 404, period: 'Makkan', verses: 60},
+		{name: 'Luqmān', 	number: 31, pageGreen: 411, period: 'Makkan', verses: 34},
+		{name: 'as-Sajdah',  	number: 32, pageGreen: 415, period: 'Makkan', verses: 30},
+		{name: 'al-Aḥzāb',  	number: 33, pageGreen: 418, period: 'Makkan', verses: 73},]
 	},
 	{
 		title: 'Part 22',
-		data: [{name: 'Sabaʾ',  	number: 34, pageGreen: 428, period: 'Makkan', verses: 99},
-		{name: 'Fāṭir',  	number: 35, pageGreen: 434, period: 'Makkan', verses: 99},
-		{name: 'Yā-Sīn',  	number: 36, pageGreen: 440, period: 'Makkan', verses: 99},]
+		data: [{name: 'Sabaʾ',  	number: 34, pageGreen: 428, period: 'Makkan', verses: 54},
+		{name: 'Fāṭir',  	number: 35, pageGreen: 434, period: 'Makkan', verses: 45},
+		{name: 'Yā-Sīn',  	number: 36, pageGreen: 440, period: 'Makkan', verses: 83},]
 	},
 	{
 		title: 'Part 23',
-		data: [{name: 'as-Ṣāffāt',  	number: 37, pageGreen: 446, period: 'Makkan', verses: 99},
-		{name: 'Ṣād',  	number: 38, pageGreen: 453, period: 'Makkan', verses: 99},
-		{name: 'az-Zumar',  	number: 39, pageGreen: 458, period: 'Makkan', verses: 99},]
+		data: [{name: 'as-Ṣāffāt',  	number: 37, pageGreen: 446, period: 'Makkan', verses: 182},
+		{name: 'Ṣād',  	number: 38, pageGreen: 453, period: 'Makkan', verses: 88},
+		{name: 'az-Zumar',  	number: 39, pageGreen: 458, period: 'Makkan', verses: 75},]
 	},
 	{
 		title: 'Part 24',
-		data: [{name: 'Ghāfir',  	number: 40, pageGreen: 467, period: 'Makkan', verses: 99},
-		{name: 'Fuṣṣilat',   	number: 41, pageGreen: 477, period: 'Makkan', verses: 99},]
+		data: [{name: 'Ghāfir',  	number: 40, pageGreen: 467, period: 'Makkan', verses: 85},
+		{name: 'Fuṣṣilat',   	number: 41, pageGreen: 477, period: 'Makkan', verses: 54},]
 	},
 	{
 		title: 'Part 25',
-		data: [{name: 'ash-Shūrā',   	number: 42, pageGreen: 483, period: 'Makkan', verses: 99},
-		{name: 'az-Zukhruf',   	number: 43, pageGreen: 489, period: 'Makkan', verses: 99},
-		{name: 'ad-Dukhān',   	number: 44, pageGreen: 496, period: 'Makkan', verses: 99},
-		{name: 'al-Jāthiyah',   	number: 45, pageGreen: 499, period: 'Makkan', verses: 99},]
+		data: [{name: 'ash-Shūrā',   	number: 42, pageGreen: 483, period: 'Makkan', verses: 53},
+		{name: 'az-Zukhruf',   	number: 43, pageGreen: 489, period: 'Makkan', verses: 89},
+		{name: 'ad-Dukhān',   	number: 44, pageGreen: 496, period: 'Makkan', verses: 59},
+		{name: 'al-Jāthiyah',   	number: 45, pageGreen: 437, period: 'Makkan', verses: 37},]
 	},
 	{
 		title: 'Part 26',
-		data: [{name: 'al-Aḥqāf',   	number: 46,  pageGreen: 502, period: 'Makkan', verses: 99},
-		{name: 'Muḥammad',   	number: 47, pageGreen: 507, period: 'Makkan', verses: 99},
-		{name: 'al-Fatḥ',   	number: 48, pageGreen: 511, period: 'Makkan', verses: 99},
-		{name: 'al-Ḥujurāt',   	number: 49, pageGreen: 515, period: 'Makkan', verses: 99},
-		{name: 'Qāf',   	number: 50, pageGreen: 518, period: 'Makkan', verses: 99},
-		{name: 'adh-Dhāriyāt',    	number: 51, pageGreen: 520, period: 'Makkan', verses: 99},]
+		data: [{name: 'al-Aḥqāf',   	number: 46,  pageGreen: 502, period: 'Makkan', verses: 35},
+		{name: 'Muḥammad',   	number: 47, pageGreen: 507, period: 'Makkan', verses: 38},
+		{name: 'al-Fatḥ',   	number: 48, pageGreen: 511, period: 'Makkan', verses: 29},
+		{name: 'al-Ḥujurāt',   	number: 49, pageGreen: 515, period: 'Makkan', verses: 18},
+		{name: 'Qāf',   	number: 50, pageGreen: 518, period: 'Makkan', verses: 45},
+		{name: 'adh-Dhāriyāt',    	number: 51, pageGreen: 520, period: 'Makkan', verses: 60},]
 	},
 	{
 		title: 'Part 27',
-		data: [{name: 'at-Ṭūr',    	number: 52, pageGreen: 523, period: 'Makkan', verses: 99},
-		{name: 'an-Najm',    	number: 53, pageGreen: 526, period: 'Makkan', verses: 99},
-		{name: 'al-Qamar',    	number: 54, pageGreen: 528, period: 'Makkan', verses: 99},
-		{name: 'ar-Raḥmān',    	number: 55, pageGreen: 531, period: 'Makkan', verses: 99},
-		{name: 'al-Wāqiʿah',    	number: 56, pageGreen: 534, period: 'Makkan', verses: 99},
-		{name: 'al-Ḥadīd',    	number: 57, pageGreen: 537, period: 'Makkan', verses: 99},]
+		data: [{name: 'at-Ṭūr',    	number: 52, pageGreen: 523, period: 'Makkan', verses: 49},
+		{name: 'an-Najm',    	number: 53, pageGreen: 526, period: 'Makkan', verses: 62},
+		{name: 'al-Qamar',    	number: 54, pageGreen: 528, period: 'Makkan', verses: 55},
+		{name: 'ar-Raḥmān',    	number: 55, pageGreen: 531, period: 'Makkan', verses: 78},
+		{name: 'al-Wāqiʿah',    	number: 56, pageGreen: 534, period: 'Makkan', verses: 96},
+		{name: 'al-Ḥadīd',    	number: 57, pageGreen: 537, period: 'Makkan', verses: 29},]
 	},
 	{
 		title: 'Part 28',
-		data: [{name: 'al-Mujādilah',    	number: 58, pageGreen: 542, period: 'Makkan', verses: 99},
-		{name: 'al-Ḥashr',    	number: 59, pageGreen: 545, period: 'Makkan', verses: 99},
-		{name: 'al-Mumtaḥanah',    	number: 60, pageGreen: 549, period: 'Makkan', verses: 99},
-		{name: 'as-Ṣaff', 	number: 61,pageGreen: 551, period: 'Makkan', verses: 99},
-		{name: 'al-Jumuʿah', 	number: 62,pageGreen: 553, period: 'Makkan', verses: 99},
-		{name: 'al-Munāfiqūn', 	number: 63,pageGreen: 554, period: 'Makkan', verses: 99},
-		{name: 'at-Taghābun', 	number: 64,pageGreen: 556, period: 'Makkan', verses: 99},
-		{name: 'at-Ṭalāq', 	number: 65,pageGreen: 558, period: 'Makkan', verses: 99},
-		{name: 'at-Taḥrīm', 	number: 66,pageGreen: 560, period: 'Makkan', verses: 99},]
+		data: [{name: 'al-Mujādilah',    	number: 58, pageGreen: 542, period: 'Makkan', verses: 22},
+		{name: 'al-Ḥashr',    	number: 59, pageGreen: 545, period: 'Makkan', verses: 24},
+		{name: 'al-Mumtaḥanah',    	number: 60, pageGreen: 549, period: 'Makkan', verses: 13},
+		{name: 'as-Ṣaff', 	number: 61,pageGreen: 551, period: 'Makkan', verses: 14},
+		{name: 'al-Jumuʿah', 	number: 62,pageGreen: 553, period: 'Makkan', verses: 11},
+		{name: 'al-Munāfiqūn', 	number: 63,pageGreen: 554, period: 'Makkan', verses: 11},
+		{name: 'at-Taghābun', 	number: 64,pageGreen: 556, period: 'Makkan', verses: 18},
+		{name: 'at-Ṭalāq', 	number: 65,pageGreen: 558, period: 'Makkan', verses: 12},
+		{name: 'at-Taḥrīm', 	number: 66,pageGreen: 560, period: 'Makkan', verses: 12},]
 	},
 	{
 		title: 'Part 29',
-		data: [{name: 'al-Mulk', 	number: 67,pageGreen: 562, period: 'Makkan', verses: 99},
-		{name: 'al-Qalam', 	number: 68,pageGreen: 564, period: 'Makkan', verses: 99},
-		{name: 'al-Ḥāqqah', 	number: 69,pageGreen: 566, period: 'Makkan', verses: 99},
-		{name: 'al-Maʿārij', 	number: 70,pageGreen: 568, period: 'Makkan', verses: 99},
-		{name: 'Nūḥ', 	number: 71,pageGreen: 570, period: 'Makkan', verses: 99},
-		{name: 'al-Jinn', 	number: 72,pageGreen: 572, period: 'Makkan', verses: 99},
-		{name: 'al-Muzzammil', 	number: 73,pageGreen: 574, period: 'Makkan', verses: 99},
-		{name: 'al-Muddaththir', 	number: 74,pageGreen: 575, period: 'Makkan', verses: 99},
-		{name: 'al-Qiyāmah', 	number: 75,pageGreen: 577, period: 'Makkan', verses: 99},
-		{name: 'al-Insān', 	number: 76,pageGreen: 578, period: 'Makkan', verses: 99},
-		{name: 'al-Mursalāt', 		number: 77,pageGreen: 580, period: 'Makkan', verses: 99},]},
+		data: [{name: 'al-Mulk', 	number: 67,pageGreen: 562, period: 'Makkan', verses: 30},
+		{name: 'al-Qalam', 	number: 68,pageGreen: 564, period: 'Makkan', verses: 52},
+		{name: 'al-Ḥāqqah', 	number: 69,pageGreen: 566, period: 'Makkan', verses: 52},
+		{name: 'al-Maʿārij', 	number: 70,pageGreen: 568, period: 'Makkan', verses: 44},
+		{name: 'Nūḥ', 	number: 71,pageGreen: 570, period: 'Makkan', verses: 28},
+		{name: 'al-Jinn', 	number: 72,pageGreen: 572, period: 'Makkan', verses: 28},
+		{name: 'al-Muzzammil', 	number: 73,pageGreen: 574, period: 'Makkan', verses: 20},
+		{name: 'al-Muddaththir', 	number: 74,pageGreen: 575, period: 'Makkan', verses: 56},
+		{name: 'al-Qiyāmah', 	number: 75,pageGreen: 577, period: 'Makkan', verses: 40},
+		{name: 'al-Insān', 	number: 76,pageGreen: 578, period: 'Makkan', verses: 31},
+		{name: 'al-Mursalāt', 		number: 77,pageGreen: 580, period: 'Makkan', verses: 50},]},
 	{
 		title: 'Part 30',
-		data: [{name: 'an-Nabaʾ', 	number: 78,pageGreen: 582, period: 'Makkan', verses: 99},
-		{name: 'an-Nāziʿāt', 	number: 79,pageGreen: 583, period: 'Makkan', verses: 99},
-		{name: 'ʿAbasa', 	number: 80,pageGreen: 585, period: 'Makkan', verses: 99},
-		{name: 'at-Takwīr', number: 81,pageGreen: 586, period: 'Makkan', verses: 99},
-		{name: 'al-Infiṭār', number: 82,pageGreen: 587, period: 'Makkan', verses: 99},
-		{name: 'al-Muṭaffifīn', number: 83,pageGreen: 587, period: 'Makkan', verses: 99},
-		{name: 'al-Inshiqāq', number: 84,pageGreen: 589, period: 'Makkan', verses: 99},
-		{name: 'al-Burūj', number: 85,pageGreen: 590, period: 'Makkan', verses: 99},
-		{name: 'at-Ṭāriq', number: 86,pageGreen: 591, period: 'Makkan', verses: 99},
-		{name: 'al-Aʿlā', number: 87,pageGreen: 591, period: 'Makkan', verses: 99},
-		{name: 'al-Ghāshiyah', number: 88,pageGreen: 592, period: 'Makkan', verses: 99},
-		{name: 'al-Fajr', number: 89,pageGreen: 593, period: 'Makkan', verses: 99},
-		{name: 'al-Balad', number: 90,pageGreen: 594, period: 'Makkan', verses: 99},
-		{name: 'ash-Shams', number: 91,pageGreen: 595, period: 'Makkan', verses: 99},
-		{name: 'al-Layl', number: 92,pageGreen: 595, period: 'Makkan', verses: 99},
-		{name: 'ad-Ḍuḥā', number: 93,pageGreen: 596, period: 'Makkan', verses: 99},
-		{name: 'ash-Sharḥ', number: 94,pageGreen: 596, period: 'Makkan', verses: 99},
-		{name: 'at-Tīn', number: 95,pageGreen: 597, period: 'Makkan', verses: 99},
-		{name: 'al-ʿAlaq', number: 96,pageGreen: 597, period: 'Makkan', verses: 99},
-		{name: 'al-Qadr', number: 97,pageGreen: 598, period: 'Makkan', verses: 99},
-		{name: 'al-Bayyinah', number: 98,pageGreen: 598, period: 'Makkan', verses: 99},
-		{name: 'az-Zalzalah', number: 99,pageGreen: 599, period: 'Makkan', verses: 99},
-		{name: 'al-ʿĀdiyāt', number: 100,pageGreen: 599, period: 'Makkan', verses: 99},
-		{name: 'al-Qāriʿah', number: 101,pageGreen: 600, period: 'Makkan', verses: 99},
-		{name: 'at-Takāthur', number: 102,pageGreen: 600, period: 'Makkan', verses: 99},
+		data: [{name: 'an-Nabaʾ', 	number: 78,pageGreen: 582, period: 'Makkan', verses: 40},
+		{name: 'an-Nāziʿāt', 	number: 79,pageGreen: 583, period: 'Makkan', verses: 46},
+		{name: 'ʿAbasa', 	number: 80,pageGreen: 585, period: 'Makkan', verses: 42},
+		{name: 'at-Takwīr', number: 81,pageGreen: 586, period: 'Makkan', verses: 29},
+		{name: 'al-Infiṭār', number: 82,pageGreen: 587, period: 'Makkan', verses: 19},
+		{name: 'al-Muṭaffifīn', number: 83,pageGreen: 587, period: 'Makkan', verses: 36},
+		{name: 'al-Inshiqāq', number: 84,pageGreen: 589, period: 'Makkan', verses: 25},
+		{name: 'al-Burūj', number: 85,pageGreen: 590, period: 'Makkan', verses: 22},
+		{name: 'at-Ṭāriq', number: 86,pageGreen: 591, period: 'Makkan', verses: 17},
+		{name: 'al-Aʿlā', number: 87,pageGreen: 591, period: 'Makkan', verses: 19},
+		{name: 'al-Ghāshiyah', number: 88,pageGreen: 592, period: 'Makkan', verses: 26},
+		{name: 'al-Fajr', number: 89,pageGreen: 593, period: 'Makkan', verses: 30},
+		{name: 'al-Balad', number: 90,pageGreen: 594, period: 'Makkan', verses: 20},
+		{name: 'ash-Shams', number: 91,pageGreen: 595, period: 'Makkan', verses: 15},
+		{name: 'al-Layl', number: 92,pageGreen: 595, period: 'Makkan', verses: 21},
+		{name: 'ad-Ḍuḥā', number: 93,pageGreen: 596, period: 'Makkan', verses: 11},
+		{name: 'ash-Sharḥ', number: 94,pageGreen: 596, period: 'Makkan', verses: 8},
+		{name: 'at-Tīn', number: 95,pageGreen: 597, period: 'Makkan', verses: 8},
+		{name: 'al-ʿAlaq', number: 96,pageGreen: 597, period: 'Makkan', verses: 19},
+		{name: 'al-Qadr', number: 97,pageGreen: 598, period: 'Makkan', verses: 5},
+		{name: 'al-Bayyinah', number: 98,pageGreen: 598, period: 'Makkan', verses: 8},
+		{name: 'az-Zalzalah', number: 8,pageGreen: 599, period: 'Makkan', verses: 8},
+		{name: 'al-ʿĀdiyāt', number: 100,pageGreen: 511, period: 'Makkan', verses: 11},
+		{name: 'al-Qāriʿah', number: 101,pageGreen: 600, period: 'Makkan', verses: 11},
+		{name: 'at-Takāthur', number: 102,pageGreen: 600, period: 'Makkan', verses: 8},
 		{
 			name: 'al-ʿAsr',number: 103,
 			pageGreen: 601,
 			period: 'Makkan',
-			verses: 99
+			verses: 3
 		},
 		{
 			name: 'al-Humazah',number: 104,
 			pageGreen: 601,
 			period: 'Makkan',
-			verses: 99
+			verses: 9
 		},
 		{
 			name: 'al-Fīl',number: 105,
 			pageGreen: 601,
 			period: 'Makkan',
-			verses: 99
+			verses: 5
 		},
 		{
 			name: 'Quraysh',number: 106,
 			pageGreen: 602,
 			period: 'Makkan',
-			verses: 99
+			verses: 4
 		},
 		{
 			name: 'al-Māʿūn',number: 107,
 			pageGreen: 602,
 			period: 'Makkan',
-			verses: 99
+			verses: 7
 		},
 		{
 			name: 'al-Kawthar',number: 108,
 			pageGreen: 602,
 			period: 'Makkan',
-			verses: 99
+			verses: 3
 		},
 		{
 			name: 'al-Kāfirūn',number: 109,
 			pageGreen: 603,
 			period: 'Makkan',
-			verses: 99
+			verses: 6
 		},
 		{
 			name: 'an-Naṣr',number: 110,
 			pageGreen: 603,
 			period: 'Makkan',
-			verses: 99
+			verses: 3
 		},
 		{
 			name: 'al-Masad',number: 111,
 			pageGreen: 603,
 			period: 'Makkan',
-			verses: 99
+			verses: 5
 		},
 		{
 			name: 'al-Ikhlāṣ',number: 112,
 			pageGreen: 604,
 			period: 'Makkan',
-			verses: 99
+			verses: 4
 		},
 		{
 			name: 'al-Falaq',number: 113,
 			pageGreen: 604,
 			period: 'Makkan',
-			verses: 99
+			verses: 5
 		},
 		{
 			name: 'an-Nās',number: 114,
 			pageGreen: 604,
 			period: 'Makkan',
-			verses: 99
+			verses: 6
 		}]
 	},
 ]
