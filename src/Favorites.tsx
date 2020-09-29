6c69bd0e00d00b6d7fc59d07ddde81767c110e37
Naved Islam <navedcoded@gmail.com>
2020-09-29 16:41:15 +0300

    basic styling

--- src/Favorites.tsx
+++ src/Favorites.tsx
@@ -1,6 +1,11 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, AsyncStorage, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, Button } from 'react-native';
 import LocalStorage from 'react-native-storage-simply';
+import Constants from 'expo-constants';
+
+
+import surahsOld from './surahsOld.js';
+
 
 function Favorites({navigation}) {
 
@@ -64,13 +69,111 @@ function Favorites({navigation}) {
     if (isLoading) {
       return <View><Text>Loading...</Text></View>;
     }
+  console.log(favorites)
   return (
     <>
-      <Text>{JSON.stringify(favorites)}</Text>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={favorites}
+          renderItem={({ item }) => <FavoriteBlock surah={item.surah} block={item.block}/> }
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
       <Button onPress={() => {clearFavorites()}} title={"clear"}></Button>
     </>
   )
 
 }
+
+function FavoriteBlock(props) {
+  let summaryData=surahsOld[props.surah].summary.breakdown[props.block]
+
+  return (
+    <>
+      <Text style={styles.header}>Favorites</Text>
+
+    <View style={styles.bodyContainer}>
+      <Text>{surahsOld[props.surah].name}</Text>
+      <Text style={styles.bodyText}>
+        {summaryData.name}
+      </Text>
+      <Text style={styles.bodyText}>
+      {summaryData.details.map((details, index) => {
+                  console.log(details[1])
+                  if (details[1] !== undefined) {
+                    return (
+                      <Text style={styles.yasirText}>
+                        {details[0] + ' (YQ. ' + details[1] + ')'}
+                      </Text>
+                    )
+                  }
+                  else {
+                    return (
+                      <Text style={styles.bodyText}>
+                        {details[0]}
+                      </Text>
+                    )
+                  }
+                })}  
+      </Text>
+      
+    </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+    backgroundColor: "#fff",
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  yasirText: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    paddingBottom: 20,
+    textAlign: 'left',
+    zIndex: 1,
+    lineHeight: 22
+  },
+  bodyContainer: {
+    margin: 20,
+    borderRadius: 8,
+    shadowColor: "#111",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    backgroundColor: '#F2FDE0',
+    paddingLeft: 22,
+    paddingRight: 22,
+    paddingBottom: 10,
+    shadowOpacity: 0.12,
+    shadowRadius: 3,
+    elevation: 5,
+    overflow: "hidden",
+  },
+  bodyText: {
+    fontSize: 17,
+    paddingBottom: 20,
+    textAlign: 'left',
+    zIndex: 1,
+    lineHeight: 22
+  },
+  header: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: 'left',
+  }
+});
 
 export default Favorites
